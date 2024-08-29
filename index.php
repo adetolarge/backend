94a68b6fbec267fb0ebd9426e16ac798f014844b
adetolarge <adetolarge@gmail.com>
2024-08-29 10:11:10 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+echo "HelloWorld!";
+// the dollar sign $ in the variable declaration. datatypes still retain their syntax except from objects.
+/** PHP is wide space insensive ie. $x = 5; this is for variable declaration 
+ * variables are declared with the dollar sign and the assignment operators in php.
+ * variables declared are case sensitve meaning that variables a and A are different. 
+ * variable names must be descriptive.
+ * never allow variabe names contain space of special characters
+ * data types include the 
+ * (integers i.e 20), (double or floating numbers), boolean(data type for true or  false), 
+ * Null (datatype with no variable), strings, arrays(datatype surrounded of box bracket) 
+ * in php we have[numeric array],[asscoiative array(they come like the same thing like javascript object but in form of array)]
+ * const variable doesn't change just like in javascript
+ * HOW SERVERS WORK
+ * They are programmed to respond to http request
+*/
+?>
