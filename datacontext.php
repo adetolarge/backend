117d26f6e5e5050ca22e5cc9ef663fae9664ee82
adetolarge <adetolarge@gmail.com>
2024-09-16 23:19:39 +0100

    update

--- datacontext.php
+++ datacontext.php
@@ -28,4 +28,20 @@ if($stmt){
     $stmt->execute();    
 }
 ?> 
-
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    <label for="id">id:</label>
+    <input type="text" id="id" name="id"><br><br>
+    <label for="seller_id">seller_id:</label>
+    <input type="text" id="seller_id" name="seller_id"><br><br>
+    <label for="title">title:</label>
+    <input type="text" id="title" name="title"><br><br>
+    <label for="description">description:</label>
+    <input type="text" id="description" name="description"><br><br>
+    <label for="rating">rating:</label>
+    <input type="text" id="rating" name="rating"><br><br>
+    <label for="image">image:</label>
+    <input type="file" id="image" name="image"><br><br>
+    <label for="price">price:</label>
+    <input type="text" id="price" name="price"><br><br>   
+    <input type="submit" value="Submit">
+</form>
