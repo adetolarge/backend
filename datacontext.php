6e166180bdca3fe8fb454f0ad228ed0879daf918
adetolarge <adetolarge@gmail.com>
2024-09-17 10:52:36 +0100

    CSRF-TOKEN

--- datacontext.php
+++ datacontext.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start()
 // $db = new mysqli("localhost", "root","", "tola_practice");
 // if ($db->connect_errno){
 //     die;
@@ -36,28 +36,36 @@ if ($db->connect_errno){
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $id = $_POST['id'];
-    $seller_id = $_POST['seller_id'];
-    $title = $_POST['title'];
-    $description = $_POST['description'];
-    $rating = $_POST['rating'];
-    $image = $_POST["image"];
-    $price = $_POST['price'];
 
-    $query = ("INSERT INTO product_name (`id`, `seller_id`, `title`, `description`, `rating`, `image`, `price`) VALUES (?, ?, ?, ?, ?, ?, ?)");
-
-    $stmt = $db->prepare($query);
-    if($stmt){
-        $stmt->bind_param('iissisi',$id, $seller_id, $title, $description, $rating, $image, $price);
-        $id;
-        $seller_id;
-        $title;
-        $description;
-        $rating;
-        $image;
-        $price;
-        $stmt->execute();    
+    if($_SESSION["CSRF-TOKEN"] != $_POST["CSRF-Token"]){
+        echo "Error Veryfying CSRF TOKEN";
+    }else{
+        $id = $_POST['id'];
+        $seller_id = $_POST['seller_id'];
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+        $rating = $_POST['rating'];
+        $image = $_POST["image"];
+        $price = $_POST['price'];
+    
+        $query = ("INSERT INTO product_name (`id`, `seller_id`, `title`, `description`, `rating`, `image`, `price`) VALUES (?, ?, ?, ?, ?, ?, ?)");
+    
+        $stmt = $db->prepare($query);
+        if($stmt){
+            $stmt->bind_param('iissisi',$id, $seller_id, $title, $description, $rating, $image, $price);
+            $id;
+            $seller_id;
+            $title;
+            $description;
+            $rating;
+            $image;
+            $price;
+            $stmt->execute();    
+        }
     }
+
+
+
 
     // if ($db) {
     //     echo "New record created successfully";
@@ -66,6 +74,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // }
 }
 $db->close();
+
+$_session["CSRF-Token"] = uniqid();
 
 
 ?>
@@ -84,7 +94,8 @@ $db->close();
     <label for="image">image:</label>
     <input type="file" id="image" name="image"><br><br>
     <label for="price">price:</label>
-    <input type="text" id="price" name="price"><br><br>   
+    <input type="text" id="price" name="price"><br><br>  
+    <input type="hidden" name="CSRF-token" value="<?php echo $_SESSION["CSRF-TOKEN"]?>"> 
     <input type="submit" value="Submit">
 </form>
 
