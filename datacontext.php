990a3cdb766e1945f9ca37225b49b9fc00737c51
adetolarge <adetolarge@gmail.com>
2024-09-18 23:08:46 +0100

    update

--- datacontext.php
+++ datacontext.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 // $db = new mysqli("localhost", "root","", "tola_practice");
 // if ($db->connect_errno){
 //     die;
@@ -29,18 +28,19 @@ session_start();
 // // ?> 
 
 <?php
+session_start();
 $db = new mysqli("localhost", "root","", "tola_practice");
 
 if ($db->connect_errno){
     die;
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+// if (isset($_POST['product_name'],$_POST["CSRF-Token"])){
 
-
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
     if ($_SESSION["CSRF-Token"] != $_POST["CSRF-Token"]){
-        echo "Error Veryfying CSRF TOKEN";
-    }else{
+        echo "Error Veryfying CSRF Token";
+    } else{
         $id = $_POST['id'];
         $seller_id = $_POST['seller_id'];
         $title = $_POST['title'];
@@ -49,6 +49,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $image = $_POST["image"];
         $price = $_POST['price'];
         // validate this data here
+        if(strlen($rating) < 1){
+            echo "rating must be between 1 - 5";
+        }
+
+        if(empty($_POST["title"])){
+            echo "Title field is required";
+        }else {
+            $title = htmlspecialchars($_POST["title"]);
+        }
+
+
+        if(empty($_POST["id"])){
+            echo "id field is required";
+        }else{
+            $id = htmlspecialchars($_POST["id"]);
+        }
+
+        if(empty($_POST["seller_id"])){
+            echo "seller_id field is required";
+        }else{
+            $seller_id = htmlspecialchars($_POST["seller_id"]);
+        }
+
         $query = ("INSERT INTO product_name (`id`, `seller_id`, `title`, `description`, `rating`, `image`, `price`) VALUES (?, ?, ?, ?, ?, ?, ?)");
     
         $stmt = $db->prepare($query);
@@ -63,14 +86,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $price;
             $stmt->execute();    
         }
+        if ($stmt) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: ";
+        }  
     }
-    // if ($db) {
-    //     echo "New record created successfully";
-    // } else {
-    //     echo "Error: ";
-    // }
-}
+    
 
+}
 $_SESSION["CSRF-Token"] = uniqid();
 
 
@@ -91,7 +115,7 @@ $_SESSION["CSRF-Token"] = uniqid();
     <input type="file" id="image" name="image"><br><br>
     <label for="price">price:</label>
     <input type="text" id="price" name="price"><br><br>  
-    <input type="hidden" id="CSRF-Token" name="CSRF-Token" value="<?php echo $_SESSION["CSRF-Token"];?>"> 
+    <input type="hidden" name="CSRF-Token" value="<?php echo $_SESSION["CSRF-Token"];?>"> 
     <input type="submit" value="Submit">
 </form>
 
